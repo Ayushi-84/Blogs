3416137e032f3d400be6073f0b702e3ec34e63a7
Ayushi-84 <kamthanayushi102@gmail.com>
2023-10-05 10:36:52 +0530

    updated the css file

--- resources/views/myBlogs.blade.php
+++ resources/views/myBlogs.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My blogs-Blog</title>
-    <link href="/css/blog.css" rel="stylesheet">
+    <link href="/css/blog.css?v=6" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
 </head>
 
