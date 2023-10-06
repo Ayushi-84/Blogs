c96a345fea4bd0c806a92ea3df88dc9798c3ca2d
Ayushi-84 <kamthanayushi102@gmail.com>
2023-10-06 18:33:44 +0530

    login page issue solved

--- resources/views/signIn.blade.php
+++ resources/views/signIn.blade.php
@@ -23,16 +23,19 @@
         <form method="POST" action="/signin"
             style="width:100%;height:71%;display:flex;justify-content:center;align-items:center;flex-direction:column">
             @csrf
-            @error('email')
                 <input name="email" id="email" type="email"
                     style="width:74%;height:15%;border:1px solid rgb(169, 169, 252);border-radius:10px;padding-right:3%;padding-left:3%;"
                     required placeholder="Enter email"
                     value="{{old('email')}}"/>
+                    @error('email')
+                    <div style="width:49%;color:red;font-size:12px">{{ $message }}</div>
             @enderror
-            @error('password')
+
                 <input name="password" id="password" type="password"
                     style="width:74%;height:15%;border:1px solid rgb(169, 169, 252);margin-top:5%;border-radius:10px;padding-right:3%;padding-left:3%;"
                     required placeholder="Enter your password" />
+                    @error('password')
+                    <div style="width:49%;color:red;font-size:12px">{{ $message }}</div>
             @enderror
             <button
                 style="width:80%;height:18%;border:1px solid #cacaca;margin-top:5%;border-radius:10px;color:white;font-size:20px;font-weight:600;font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;background-color:rgb(53, 53, 252);">Log
